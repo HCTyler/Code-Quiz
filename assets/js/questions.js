5233b0b357f6f302a9c472ae6b953e4b6471dcf8
Edgar Fernandez <edgar.fernand@outlook.com>
2021-11-19 00:36:26 -0800

    added content; worked on background video and container

---
+++ assets/js/questions.js
@@ -0,0 +1,10 @@
+var easyQuestions = [
+    {
+        title: "Which type of blood cells helps regulate body temperature and carries oxygen?",
+        choices:["Red Blood Cells", "White Blood Cells", "Plateles"],
+        answer:"Red Blood Cells"
+
+    },
+    
+
+]
